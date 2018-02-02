aef29cc9ca2f9a27ed3f413bfc8e15d084e5a41b
ocph23 <ocph23@hotmail.com>
2018-02-02 15:26:30 +0900

    #lastCommit

---
+++ FisikaWithMultimedia/FisikaWithMultimedia/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using FirstFloor.ModernUI.Windows.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FisikaWithMultimedia
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow :Window
+    {
+        private double skala;
+        private double detik;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            kecepatan.Text = (Convert.ToDouble(jarak.Text) / Convert.ToDouble(waktu.Text)).ToString() + "km/Jam";
+            TranslateTransform trans = new TranslateTransform();
+            ex2.RenderTransform = trans;
+           // DoubleAnimation anim1 = new DoubleAnimation(top, 0 - top, TimeSpan.FromSeconds(10));
+            DoubleAnimation anim2 = new DoubleAnimation(ex2.Width/2, border1.Width, TimeSpan.FromSeconds(Convert.ToDouble(waktu.Text) * detik));
+          //  trans.BeginAnimation(TranslateTransform.XProperty, anim1);
+            trans.BeginAnimation(TranslateTransform.XProperty, anim2);
+        }
+
+        private void Trans_Changed(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Ex2_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            this.skala = Convert.ToDouble(skalacm.Text) * 37.795275590551178;
+            border1.Width = Convert.ToDouble(jarak.Text) * skala;
+            this.detik = Convert.ToDouble(skalaw.Text) * 1;
+
+            var w = System.Windows.SystemParameters.PrimaryScreenWidth;
+        }
+    }
+}
---
+++ FisikaWithMultimedia/FisikaWithMultimedia/Parabola.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Shapes;
+
+namespace FisikaWithMultimedia
+{
+    /// <summary>
+    /// Interaction logic for Parabola.xaml
+    /// </summary>
+    public partial class Parabola : Window
+    {
+        public Parabola()
+        {
+            InitializeComponent();
+
+            DoubleAnimation anim2 = new DoubleAnimation(0,360, TimeSpan.FromSeconds(2));
+           // anim2.AutoReverse = true;
+            anim2.RepeatBehavior = RepeatBehavior.Forever;
+            //  trans.BeginAnimation(TranslateTransform.XProperty, anim1);
+            TranslateTransform trans = new TranslateTransform();
+            //rotate.BeginAnimation(AxisAngleRotation3D.AngleProperty, anim2);
+            trans.BeginAnimation(TranslateTransform.XProperty, anim2);
+            //rotate.Angle = 30;
+        }
+    }
+}
